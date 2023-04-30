a47413db8bcaefd92e69f81e86095fae26c49633
Mohammad-Alismael <mhdhd260@gmail.com>
2023-04-30 16:21:42 +0400

    Initial commit

---
+++ my-app/app/chat/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Page.propTypes = {
+
+};
+
+function Page(props) {
+    return (
+        <div>
+            this is chat page
+        </div>
+    );
+}
+
+export default Page;
